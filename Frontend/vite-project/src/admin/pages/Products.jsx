41c059506bbfca1e559583a5f418a5ed92287aff
Mohammad Danish <danish.sab05@gmail.com>
2026-06-27 23:42:05 +0530

    feat:add products page in admin side

---
+++ Frontend/vite-project/src/admin/pages/Products.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layout/AdminLayout";
+
+const Products = () => {
+  return (
+    <AdminLayout>
+      <h1>Products</h1>
+    </AdminLayout>
+  );
+};
+
+export default Products;
