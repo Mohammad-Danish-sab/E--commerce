d42dd33f7ad392dc1151954b99731e8b24c15582
Mohammad Danish <danish.sab05@gmail.com>
2026-06-28 18:57:03 +0530

    feat:add product page

---
+++ Frontend/vite-project/src/admin/pages/AddProduct.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layout/AdminLayout";
+
+const AddProduct = () => {
+  return (
+    <AdminLayout>
+      <h1>Add Product</h1>
+    </AdminLayout>
+  );
+};
+
+export default AddProduct;
