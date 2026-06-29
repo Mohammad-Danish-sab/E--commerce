535e5e25ce046255abd1241412374760ae3ec0af
Mohammad Danish <danish.sab05@gmail.com>
2026-06-30 01:08:32 +0530

    feat:add category page

---
+++ Frontend/vite-project/src/admin/pages/Categories.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layout/AdminLayout";
+
+const Categories = () => {
+  return (
+    <AdminLayout>
+      <h1>Categories</h1>
+    </AdminLayout>
+  );
+};
+
+export default Categories;
