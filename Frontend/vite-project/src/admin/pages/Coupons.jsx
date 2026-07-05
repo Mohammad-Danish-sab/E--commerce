fc23aaa2390217f275f80c1c7295148f94ce03cd
Mohammad Danish <danish.sab05@gmail.com>
2026-07-05 23:29:17 +0530

    feat:add the admin dashboard

---
+++ Frontend/vite-project/src/admin/pages/Coupons.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layout/AdminLayout";
+
+const Coupons = () => {
+  return (
+    <AdminLayout>
+      <h1>Coupons</h1>
+    </AdminLayout>
+  );
+};
+
+export default Coupons;
---
+++ Frontend/vite-project/src/admin/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../layout/AdminLayout";
+
+const Settings = () => {
+  return (
+    <AdminLayout>
+      <h1>Settings</h1>
+    </AdminLayout>
+  );
+};
+
+export default Settings;
